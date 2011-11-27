cbede1ae17472ad75c66146c9085dffee733e6b0
Francois Coulombe <fcoulombe@silentfalls.org>
2011-11-27 12:32:10 -0800

    franky fixes the indentation

--- lib/voxel/unittest/main.cpp
+++ lib/voxel/unittest/main.cpp
@@ -3,9 +3,9 @@
 
 #include <SDL.h>
 
-#include "Voxel/Cube.h"
-#include "Voxel/Grid.h"
-#include "Voxel/Voxel.h"
+#include "voxel/Cube.h"
+#include "voxel/Grid.h"
+#include "voxel/Voxel.h"
 
 #include "GridTest.h"
 int main(int /*argc*/, char ** argv)

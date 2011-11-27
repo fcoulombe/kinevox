1adf0a134ea2bbaf28bf6d2719040e03cf1b3e46
fcoulombe <fcoulombe@silentfalls.org>
2011-11-27 11:20:45 -0800

    franky adds proper texture coordinate to the renderobejct

--- lib/applayer/GCLRenderObject.cpp
+++ lib/applayer/GCLRenderObject.cpp
@@ -5,14 +5,15 @@ using namespace GCL;
 
 const   VertexPNT GCLRenderObject::square[4] = {
 		{WorldPoint3(-0.5, -0.5, 0.0), 	WorldPoint3(0.0, 0.0, 1.0) ,WorldPoint2(0.0, 0.0)},
-		{WorldPoint3(0.5, -0.5, 0.0), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(0.0, 0.0)},
-		{WorldPoint3(-0.5, 0.5, 0.0), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(0.0, 0.0)},
-		{WorldPoint3(0.5, 0.5, 0.0), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(0.0, 0.0)}};
+		{WorldPoint3(0.5, -0.5, 0.0), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(1.0, 0.0)},
+		{WorldPoint3(-0.5, 0.5, 0.0), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(0.0, 1.0)},
+		{WorldPoint3(0.5, 0.5, 0.0), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(1.0, 1.0)}};
 const VertexData GCLRenderObject::data = {&square, 4, VertexPNT::GetComponentType()};
 
 
 GCLRenderObject::GCLRenderObject()
-: RenderObject(Matrix44::IDENTITY)
+: RenderObject(Matrix44::IDENTITY),
+  mMaterial("Default")
 {
 	GCLApplication::RegisterRenderObject(this);
 }

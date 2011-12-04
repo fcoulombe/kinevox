1fbd601d62851738994221282e8a86a615f6ed51
fcoulombe <fcoulombe@silentfalls.org>
2011-12-03 20:22:44 -0800

    franky cleans up a bit the way the errors are being reported

--- lib/renderer/renderer/OpenGL.h
+++ lib/renderer/renderer/OpenGL.h
@@ -19,7 +19,7 @@
 
 
 #include <gcl/Assert.h>
-#include <iostream>
+#include <sstream>
 #define glErrorCheck() do { i_glErrorCheck(__FILE__, __LINE__); }while(0);
 inline void i_glErrorCheck(const char *file, int line)
 {
@@ -27,32 +27,34 @@ inline void i_glErrorCheck(const char *file, int line)
 	if (err == GL_NO_ERROR)
 		return;
 
-	std::cerr << file << ":" <<  line << ": OpenGL Error" << std::endl;
+	std::stringstream s;
+	s<<file << ":" <<  line << ": OpenGL Error" << std::endl;
 	switch (err)
 	{
 	case GL_INVALID_ENUM:
-	  GCLAssert(false &&"An unacceptable value is specified for an enumerated argument. The offending command is ignored and has no other side effect than to set the error flag.");
+	  s<<"An unacceptable value is specified for an enumerated argument. The offending command is ignored and has no other side effect than to set the error flag.";
 		break;
 	case GL_INVALID_VALUE:
-	  GCLAssert(false && "A numeric argument is out of range. The offending command is ignored and has no other side effect than to set the error flag.");
+	  s<<"A numeric argument is out of range. The offending command is ignored and has no other side effect than to set the error flag.";
 		break;
 	case GL_INVALID_OPERATION:
-	  GCLAssert(false && "The specified operation is not allowed in the current state. The offending command is ignored and has no other side effect than to set the error flag.");
+	 s<<"The specified operation is not allowed in the current state. The offending command is ignored and has no other side effect than to set the error flag.";
 		break;
 	case GL_STACK_OVERFLOW:
-	  GCLAssert(false && "This command would cause a stack overflow. The offending command is ignored and has no other side effect than to set the error flag.");
+	  s<<"This command would cause a stack overflow. The offending command is ignored and has no other side effect than to set the error flag.";
 		break;
 	case GL_STACK_UNDERFLOW:
-	  GCLAssert(false && "This command would cause a stack underflow. The offending command is ignored and has no other side effect than to set the error flag.");
+	  s<<"This command would cause a stack underflow. The offending command is ignored and has no other side effect than to set the error flag.";
 		break;
 	case GL_OUT_OF_MEMORY:
-	  GCLAssert(false && "There is not enough memory left to execute the command. The state of the GL is undefined, except for the state of the error flags, after this error is recorded.");
+	  s<<"There is not enough memory left to execute the command. The state of the GL is undefined, except for the state of the error flags, after this error is recorded.";
 		break;
 	case GL_TABLE_TOO_LARGE:
-	  GCLAssert(false && "The specified table exceeds the implementation's maximum supported table size. The offending command is ignored and has no other side effect than to set the error flag.");
+	  s<< "The specified table exceeds the implementation's maximum supported table size. The offending command is ignored and has no other side effect than to set the error flag.";
 		break;
 	default:
-	  GCLAssert(false && "OpenGL: don't know what happenedl");
+	  s<<"OpenGL: don't know what happenedl";
 	}
+	GCLAssertMsg(false, s.str().c_str());
 
 }

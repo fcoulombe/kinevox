83460ec77ffb6b43a1131e1c811ca512557ea29f
fcoulombe <fcoulombe@DoomMachine.(none)>
2012-02-19 17:53:14 -0600

    franky turns the square into what should be a cube. its still a bit flaky

--- lib/applayer/GCLRenderObject.cpp
+++ lib/applayer/GCLRenderObject.cpp
@@ -3,12 +3,45 @@
 
 using namespace GCL;
 
-const   VertexPNT GCLRenderObject::square[4] = {
-		{WorldPoint3(-0.5, -0.5, 0.0), 	WorldPoint3(0.0, 0.0, 1.0) ,WorldPoint2(0.0, 0.0)},
-		{WorldPoint3(0.5, -0.5, 0.0), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(1.0, 0.0)},
-		{WorldPoint3(-0.5, 0.5, 0.0), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(0.0, 1.0)},
-		{WorldPoint3(0.5, 0.5, 0.0), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(1.0, 1.0)}};
-const VertexData GCLRenderObject::data = {&square, 4, VertexPNT::GetComponentType()};
+const   VertexPNT GCLRenderObject::cube[GCLRenderObject::NUM_VERTICES] = {
+		//front
+		{WorldPoint3(-0.5, -0.5, 0.5), 	WorldPoint3(0.0, 0.0, 1.0) ,WorldPoint2(0.0, 0.0)},
+		{WorldPoint3(0.5, -0.5, 0.5), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(1.0, 0.0)},
+		{WorldPoint3(-0.5, 0.5, 0.5), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(0.0, 1.0)},
+		{WorldPoint3(0.5, 0.5, 0.5), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(1.0, 1.0)},
+
+		//back
+		{WorldPoint3(-0.5, -0.5, -0.5), WorldPoint3(0.0, 0.0, 1.0) ,WorldPoint2(0.0, 0.0)},
+		{WorldPoint3(0.5, -0.5, -0.5), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(1.0, 0.0)},
+		{WorldPoint3(-0.5, 0.5, -0.5), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(0.0, 1.0)},
+		{WorldPoint3(0.5, 0.5, -0.5), 	WorldPoint3(0.0, 0.0, 1.0), WorldPoint2(1.0, 1.0)},
+
+		//left
+		{WorldPoint3(-0.5, -0.5, -0.5), WorldPoint3(-1.0, 0.0, 0.0) ,WorldPoint2(0.0, 0.0)},
+		{WorldPoint3(-0.5, -0.5, 0.5), 	WorldPoint3(-1.0, 0.0, 0.0), WorldPoint2(1.0, 0.0)},
+		{WorldPoint3(-0.5, 0.5, -0.5), 	WorldPoint3(-1.0, 0.0, 0.0), WorldPoint2(0.0, 1.0)},
+		{WorldPoint3(-0.5, 0.5, 0.5), 	WorldPoint3(-1.0, 0.0, 0.0), WorldPoint2(1.0, 1.0)},
+
+		//right
+		{WorldPoint3(0.5, -0.5, -0.5), WorldPoint3(-1.0, 0.0, 0.0) ,WorldPoint2(0.0, 0.0)},
+		{WorldPoint3(0.5, -0.5, 0.5), 	WorldPoint3(-1.0, 0.0, 0.0), WorldPoint2(1.0, 0.0)},
+		{WorldPoint3(0.5, 0.5, -0.5), 	WorldPoint3(-1.0, 0.0, 0.0), WorldPoint2(0.0, 1.0)},
+		{WorldPoint3(0.5, 0.5, 0.5), 	WorldPoint3(-1.0, 0.0, 0.0), WorldPoint2(1.0, 1.0)},
+
+		//top
+		{WorldPoint3(0.5, 0.5, 0.5), WorldPoint3(0.0, 1.0, 0.0) ,WorldPoint2(1.0, 0.0)},
+		{WorldPoint3(0.5, 0.5, -0.5), 	WorldPoint3(0.0, 1.0, 0.0), WorldPoint2(0.0, 0.0)},
+		{WorldPoint3(-0.5, 0.5, -0.5), 	WorldPoint3(0.0, 1.0, 0.0), WorldPoint2(0.0, 1.0)},
+		{WorldPoint3(-0.5, 0.5, 0.5), 	WorldPoint3(0.0, 1.0, 0.0), WorldPoint2(1.0, 1.0)},
+
+		//bottom
+		{WorldPoint3(0.5, -0.5, 0.5), WorldPoint3(0.0, 1.0, 0.0) ,WorldPoint2(1.0, 0.0)},
+		{WorldPoint3(0.5, -0.5, -0.5), 	WorldPoint3(0.0, 1.0, 0.0), WorldPoint2(0.0, 0.0)},
+		{WorldPoint3(-0.5,-0.5, -0.5), 	WorldPoint3(0.0, 1.0, 0.0), WorldPoint2(0.0, 1.0)},
+		{WorldPoint3(-0.5, -0.5, 0.5), 	WorldPoint3(0.0, 1.0, 0.0), WorldPoint2(1.0, 1.0)}
+
+};
+const VertexData GCLRenderObject::data(&cube, GCLRenderObject::NUM_VERTICES, VertexPNT::GetComponentType());
 
 
 GCLRenderObject::GCLRenderObject()

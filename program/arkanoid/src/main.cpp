eb7783a6d6a18380c61beb75733a480fa37c130d
fcoulombe <fcoulombe@silentfalls.org>
2012-02-23 11:36:42 -0600

    franky starts implementing the arkanoid game by creating the basic game objects

--- program/arkanoid/src/main.cpp
+++ program/arkanoid/src/main.cpp
@@ -24,9 +24,10 @@
 #include <renderer/OpenCV.h>
 #include <SDL.h>
 
-#include <applayer/GCLSprite.h>
 #include <applayer/GCLApplication.h>
+#include <applayer/GCLRenderObject2D.h>
 #include <gcl/Exception.h>
+#include <gcl/Point3.h>
 #include <input/Input.h>
 #include <renderer/Camera.h>
 #include <renderer/Material.h>
@@ -35,6 +36,72 @@
 
 using namespace GCL;
 
+static const size_t NUM_ROW = 10;
+static const size_t NUM_COL = 20;
+static const size_t BORDER_SIZE = 5;
+
+class AGameObject
+{
+public:
+	AGameObject(const char *objSpriteName)
+	: mRenderObject(objSpriteName)
+	{
+		mRenderObject.SetScale(WorldPoint2(0.5, 0.5));
+	}
+	void SetPosition(const WorldPoint3 &position) { mRenderObject.SetPosition(position); }
+	const WorldPoint3 &GetPosition() const { return mRenderObject.GetPosition(); }
+protected:
+	GCLRenderObject2D mRenderObject;
+
+};
+
+class Block : public AGameObject
+{
+public:
+	Block(size_t blockIndex)
+	: AGameObject("Block")
+	{
+		const ViewPort &viewPort = GCLApplication::GetRenderer()->GetViewPort();
+
+		size_t spaceX = viewPort.GetWidth()/NUM_COL;
+		size_t spaceY = viewPort.GetHeight()/NUM_ROW;
+(void)spaceX;
+(void)spaceY;
+		size_t y = blockIndex/NUM_ROW;
+		size_t x = blockIndex%NUM_ROW;
+
+		WorldPoint3 position(x, y, 0.0);
+		SetPosition(position);
+		std::cout << position << std::endl;
+	}
+private:
+};
+
+class Paddle : public AGameObject
+{
+public:
+	Paddle()
+	: AGameObject("Paddle")
+	{
+		SetPosition(WorldPoint3(640.0, 500.0, 0.0));
+	}
+};
+
+
+class Ball : public AGameObject
+{
+public:
+	Ball()
+	: AGameObject("Ball")
+	{
+		SetPosition(WorldPoint3(640.0, 500.0, 0.0));
+	}
+	void Update()
+	{
+		SetPosition(GetPosition() + mVelocity);
+	}
+	WorldPoint3 mVelocity;
+};
 
 int main(int /*argc*/, char ** /*argv*/)
 {
@@ -47,49 +114,35 @@ int main(int /*argc*/, char ** /*argv*/)
 		GCLApplication::SetViewportCamera(myCamera);
 
 
-		GCLSprite obj("Paddle");
+		typedef std::vector<Block *> BlockList;
+		BlockList blocks;
+		for (size_t i=0; i<blocks.size(); ++i)
+		{
+			blocks.push_back(new Block(i));
+		}
+		Paddle paddle;
+		Ball ball;
 
 
 		bool isRunning = true;
-		Real x = 0.0;
-		Real y = 0.0;
 		while (isRunning)
 		{
+			ball.Update();
+
 			GCLApplication::Update();
 			if (Input::IsKeyUp(SDLK_ESCAPE))
 				isRunning=false;
 
-			if (Input::IsKeyUp(SDLK_UP))
-				y+=1.;
-			if (Input::IsKeyUp(SDLK_DOWN))
-				y-=1.;
-
+			WorldPoint3 tempPosition;
+			tempPosition= paddle.GetPosition();
 			if (Input::IsKeyUp(SDLK_LEFT))
-				x-=1.;
+				tempPosition.x -=5;
+
 			if (Input::IsKeyUp(SDLK_RIGHT))
-				x+=1.;
+				tempPosition.x+=5.;
 
-			if (Input::IsKeyUp(SDLK_PAGEUP))
-				myCamera.TiltUp();
-			if (Input::IsKeyUp(SDLK_PAGEDOWN))
-				//myCamera.TiltDown();
-			{
-				static bool isTextureOn = false;
-				isTextureOn = !isTextureOn;
-				if (isTextureOn)
-				{
-					std::cout << "on" << std::endl;
-					GCLApplication::GetRenderer()->mCurrentRenderState.SetTextureEnabled(true);
-				}
-				else
-				{
-					std::cout << "off" << std::endl;
-					GCLApplication::GetRenderer()->mCurrentRenderState.SetTextureEnabled(false);
-				}
-			}
+			paddle.SetPosition(tempPosition);
 
-
-			obj.SetPosition(WorldPoint3(x,y,0.0));
 			GCLApplication::Render();
 			usleep(100);
 			std::cout.flush();

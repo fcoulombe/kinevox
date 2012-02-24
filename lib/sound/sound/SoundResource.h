66aa732e358ba767f98fddfa2ddc5ce9bb978292
fcoulombe <fcoulombe@silentfalls.org>
2012-02-23 20:44:20 -0600

    franky adds resource stubs for the sound resource and the wav loading code

---
+++ lib/sound/sound/SoundResource.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2011 by Francois Coulombe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <gcl/Resource.h>
+
+namespace GCL
+{
+  class SoundResource : public Resource
+  {
+    public:
+
+    SoundResource(const char *soundName);
+    ~SoundResource();
+
+    struct SoundData
+    {
+      SoundData()
+      {
+        soundData = NULL;
+      }
+      ~SoundData()
+      {
+
+      }
+
+      GLubyte *imageData;
+    }mSoundData;
+
+    static const SoundResource EmptySound;
+
+    static void LoadWav(std::istream &is, SoundData &data);
+    static void Unload(SoundData &data);
+  private:
+    SoundResource() {}
+  };
+}

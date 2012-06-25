8febdbfa60dff5a71c30b03d8aa8edecdbbed294
fcoulombe <fcoulombe@silentfalls.org>
2012-06-24 18:07:35 -0600

    franky updates the order of the member data to adjust for the padding

--- program/tools/common/unittest/BufferWriterTest.h
+++ program/tools/common/unittest/BufferWriterTest.h
@@ -23,6 +23,7 @@
 #include <fstream>
 #include <sstream>
 #include <cstring>
+#include <iomanip>
 
 #include <common/BufferWriter.h>
 #include <gcl/File.h>
@@ -34,23 +35,24 @@ namespace BufferWriterTest
 
 struct TestData
 {
-	uint32_t val1;
-	uint32_t val2;
-	uint16_t val3;
-	uint16_t val4;
-	WorldPoint2 p2;
-	WorldPoint3 p3;
-	WorldPoint4 p4;
+	WorldPoint4 p4; //0
+	WorldPoint3 p3; //36
+	WorldPoint2 p2; //60
+	uint32_t val1; //76
+	uint32_t val2; //80
+	uint16_t val3; //82
+	uint16_t val4; //84
 };
 BufferWriter &operator<<(BufferWriter &buffer, const TestData &data)
 {
+
+	buffer << data.p4;
+	buffer << data.p3;
+	buffer << data.p2;
 	buffer.Write(data.val1);
 	buffer.Write(data.val2);
 	buffer.Write(data.val3);
 	buffer.Write(data.val4);
-	buffer << data.p2;
-	buffer << data.p3;
-	buffer << data.p4;
 	return buffer;
 }
 void Test()
@@ -58,26 +60,51 @@ void Test()
 	TEST_START
 	std::stringstream s;
 
+	//test get padding size
+	{
+		size_t paddingSize = BufferWriter::GetPaddingSize();
+#if USE_64BIT_PLATFORM
+		Assert_Test(paddingSize == 8);
+#else
+		Assert_Test(paddingSize == 4);
+#endif
+	}
 	//test stream in
 	{
 		BufferWriter buffer(4096);
 		TestData data;
-		data.val1 = 0;
-		data.val2 = 1;
-		data.val3 = 2;
-		data.val4 = 3;
-		data.p2.x = 1.5;
-		data.p2.y = 1.7;
-		data.p3.x = 1.5;
-		data.p3.y = 1.7;
-		data.p3.z = 1.0;
+		memset( &data, 0xfe,sizeof(TestData));
 		data.p4.x = 1.5;
 		data.p4.y = 1.7;
 		data.p4.z = 1.0;
 		data.p4.w = 0.5;
+		data.p3.x = 1.5;
+		data.p3.y = 1.7;
+		data.p3.z = 1.0;
+		data.p2.x = 1.5;
+		data.p2.y = 1.7;
+		data.val4 = 3;
+		data.val3 = 2;
+		data.val2 = 1;
+		data.val1 = 666;
 
 		buffer << data;
-		Assert_Test(buffer.GetCurrentOffset() == sizeof(TestData));
+		buffer.Pad();
+
+		s.str("");
+		s<<std::setprecision(16)<< std::endl<<buffer.GetCurrentOffset()<< std::endl <<"==" << std::endl << sizeof(TestData);
+		AssertMsg_Test(buffer.GetCurrentOffset() == sizeof(TestData), s.str().c_str());
+
+		for (size_t i=0; i<sizeof(TestData); ++i)
+		{
+			s.str("");
+			s<<std::endl<<"byte: " << i << " " << (int)buffer.GetBuffer()[i]  <<" == " << (int)((const uint8_t*)&data)[i];
+			AssertMsg_Test((int)(buffer.GetBuffer()[i]) == (int)(((const uint8_t*)&data)[i]), s.str().c_str());
+		}
+		Assert_Test(memcmp(buffer.GetBuffer(), (const char*)&data,sizeof(TestData))==0);
+
+		//s.str("");
+		//s<<std::setprecision(16)<< std::endl<<GLRenderer::GetGLProjection() << std::endl <<"==" << std::endl << camera.GetProjection();
 		Assert_Test(memcmp(buffer.GetBuffer(), (const char*)&data,sizeof(TestData))==0);
 
 
@@ -92,8 +119,8 @@ void Test()
 		buffer.Write(pad);
 		Assert_Test(buffer.GetCurrentOffset() == sizeof(uint8_t));
 		buffer.Pad();
-		s<<buffer.GetCurrentOffset() << " == " << sizeof(uint32_t) << std::endl;
-		AssertMsg_Test(buffer.GetCurrentOffset() == sizeof(uint32_t), s.str().c_str());
+		s<<buffer.GetCurrentOffset() << " == " << BufferWriter::GetPaddingSize() << std::endl;
+		AssertMsg_Test(buffer.GetCurrentOffset() == BufferWriter::GetPaddingSize(), s.str().c_str());
 	}
 	//test padding when no need to
 	{
@@ -102,8 +129,8 @@ void Test()
 		buffer.Write(pad);
 		Assert_Test(buffer.GetCurrentOffset() == sizeof(uint32_t));
 		buffer.Pad();
-		s<<buffer.GetCurrentOffset() << " == " << sizeof(uint32_t) << std::endl;
-		AssertMsg_Test(buffer.GetCurrentOffset() == sizeof(uint32_t), s.str().c_str());
+		s<<buffer.GetCurrentOffset() << " == " << BufferWriter::GetPaddingSize() << std::endl;
+		AssertMsg_Test(buffer.GetCurrentOffset() == BufferWriter::GetPaddingSize(), s.str().c_str());
 	}
 
 	//string writing test

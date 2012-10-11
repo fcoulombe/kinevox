20772c51415f748c9974fad11872dd0c15b43f88
fcoulombe <fcoulombe@silentfalls.org>
2012-10-11 14:02:39 -0500

    franky adds a few headers to the 3rdparty freetype header

--- 3rdParty/include/3rdparty/FreeType.h
+++ 3rdParty/include/3rdparty/FreeType.h
@@ -30,9 +30,12 @@
 #			pragma GCC system_header
 #		endif
 #	endif
+
 #include <ft2build.h>
 #include FT_FREETYPE_H
-
+#include FT_GLYPH_H
+#include FT_OUTLINE_H
+#include FT_BITMAP_H
 
 
 #undef __FTERRORS_H__

ab399aeecf7af5a1c11e4652bb703af553871d5f
fcoulombe <fcoulombe@silentfalls.org>
2013-04-01 23:02:57 -0300

    franky ports the frame buffer to openrl

--- lib/renderer/RLFrameBuffer.cpp
+++ lib/renderer/RLFrameBuffer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 by Francois Coulombe
+ * Copyright (C) 2013 by Francois Coulombe
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,74 +20,57 @@
  * THE SOFTWARE.
  */
 
-#include "renderer/FrameBuffer.h"
+#include "renderer/RLFrameBuffer.h"
 
 #include <sstream>
 
-#include "renderer/RenderBuffer.h"
-#include "renderer/Texture.h"
+//#include "renderer/RenderBuffer.h"
+#include "renderer/RLTexture.h"
 
 using namespace GCL;
 
 namespace
 {
-void checkFrameBufferStatus(GLenum stat)
+void checkFrameBufferStatus(RLenum stat)
 {
-	if (GL_FRAMEBUFFER_COMPLETE == stat)
+	if (RL_FRAMEBUFFER_COMPLETE == stat)
 		return;
-#if defined(ES1) || defined(ES2)
-	GCLAssert(GL_FRAMEBUFFER_COMPLETE == stat);
-#else
+
 	std::stringstream s;
 	switch (stat)
 	{
-	case GL_FRAMEBUFFER_UNDEFINED:
-		s << "GL_FRAMEBUFFER_UNDEFINED is returned if target is the default framebuffer, but the default framebuffer does not exist.";
+	case RL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS:
+		s << "RL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS is returned if the z-offset exceeds any attached texture depth or the frame buffer width and height are not within all attached texture width and height values.";
 		break;
-	case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT :
-		s << "GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT is returned if any of the framebuffer attachment points are framebuffer incomplete.";
+	case RL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT :
+		s << "RL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT is returned if any of the framebuffer attachment points are framebuffer incomplete.";
 		break;
-	case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
-		s<< "GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT is returned if the framebuffer does not have at least one image attached to it.";
+	case RL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+		s<< "RL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT is returned if the framebuffer does not have at least one image attached to it.";
 		break;
-	case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
-		s << "GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER is returned if the value of GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE is GL_NONE for any color attachment point(s) named by GL_DRAWBUFFERi.";
+	case RL_FRAMEBUFFER_INCOMPLETE_FORMATS :
+		s << "RL_FRAMEBUFFER_INCOMPLETE_FORMATS is returned if any attached texture has an unsupported format.";
 		break;
-	case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
-		s << "GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER is returned if GL_READ_BUFFER is not GL_NONE and the value of GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE is GL_NONE for the color attachment point named by GL_READ_BUFFER.";
-		break;
-	case GL_FRAMEBUFFER_UNSUPPORTED:
-		s << "GL_FRAMEBUFFER_UNSUPPORTED is returned if the combination of internal formats of the attached images violates an implementation-dependent set of restrictions.";
-		break;
-	case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
-		s << "GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE is returned if the value of GL_RENDERBUFFER_SAMPLES is not the same for all attached renderbuffers; if the value of GL_TEXTURE_SAMPLES is the not same for all attached textures; or, if the attached images are a mix of renderbuffers and textures, the value of GL_RENDERBUFFER_SAMPLES does not match the value of GL_TEXTURE_SAMPLES.";
-		s << std::endl;
-		s << "GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE is also returned if the value of GL_TEXTURE_FIXED_SAMPLE_LOCATIONS is not the same for all attached textures; or, if the attached images are a mix of renderbuffers and textures, the value of GL_TEXTURE_FIXED_SAMPLE_LOCATIONS is not GL_TRUE for all attached textures.";
-		break;
-		/* case  GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS:
-		  s<< "GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS is returned if any framebuffer attachment is layered, and any populated attachment is not layered, or if all populated color attachments are not from textures of the same target.";*/
 	default:
 		s<<"don't know what happenned to the framebuffer";
 	}
     
-	GCLAssertMsg(GL_FRAMEBUFFER_COMPLETE == stat, s.str().c_str());
-#endif
-
+	GCLAssertMsg(RL_FRAMEBUFFER_COMPLETE == stat, s.str().c_str());
 }
 }
 
-FrameBuffer::FrameBuffer(const Texture & texture, const RenderBuffer & depthBuffer)
-: mFrameBufferId((GLuint)-1)
+RLFrameBuffer::RLFrameBuffer(const RLTexture & texture/*, const RenderBuffer & depthBuffer*/)
+: mFrameBufferId((RLframebuffer)-1)
 {
 	GCLAssert(texture.IsValid());
 
-	glGenFramebuffers(1, &mFrameBufferId); glErrorCheck();
-	glBindFramebuffer(GL_FRAMEBUFFER, mFrameBufferId); glErrorCheck();
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture.GetTextureId(), 0); glErrorCheck();
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBuffer.GetRenderBufferId());glErrorCheck();
+	rlGenFramebuffers(1, &mFrameBufferId); 
+	rlBindFramebuffer(RL_FRAMEBUFFER, mFrameBufferId); 
+	rlFramebufferTexture2D(RL_FRAMEBUFFER, RL_COLOR_ATTACHMENT0, RL_TEXTURE_2D, texture.GetTextureId(), 0); 
+	//rlFramebufferRenderbuffer(RL_FRAMEBUFFER, RL_DEPTH_ATTACHMENT, RL_RENDERBUFFER, depthBuffer.GetRenderBufferId());
 
-	GLenum status =  glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	RLenum status =  rlCheckFramebufferStatus(RL_FRAMEBUFFER);
 	checkFrameBufferStatus(status);
 
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);glErrorCheck();
+	rlBindFramebuffer(RL_FRAMEBUFFER, 0);
 }

5899c7fefedc367affc22a2026275b1718fbb244
fcoulombe <fcoulombe@silentfalls.org>
2013-06-04 10:46:35 -0300

    franky ports the find package to work under msys

--- 3rdParty/libpng/Findlibpng.cmake
+++ 3rdParty/libpng/Findlibpng.cmake
@@ -1,29 +1,28 @@
 IF(${WIN32})
-FIND_PACKAGE(zlib)
+	FIND_PACKAGE(zlib)
+	IF(${IS_MSVC})
+		SET(PNG_ROOT "${PROJECT_SOURCE_DIR}/3rdParty/libpng/")
+		SET(LPATH ${PNG_ROOT}/lib/${KINEVOX_ARCHITECTURE})
+	ELSEIF(${IS_GNU})
+		SET(PNG_ROOT "${MSYS_PATH}/local")		
+		SET(LPATH ${PNG_ROOT}/lib/)
+	ENDIF()
 
-SET(PNG_ROOT "${PROJECT_SOURCE_DIR}/3rdParty/libpng/")
-FIND_PATH(
-  PNG_INCLUDE_DIR png.h
-  PATHS ${PNG_ROOT}/include)
+	SET(PNG_ROOT "${PROJECT_SOURCE_DIR}/3rdParty/libpng/")
+	FIND_PATH(
+		  PNG_INCLUDE_DIR png.h
+		  PATHS ${PNG_ROOT}/include)
+		
+	FindLibrary(PNG_LIBRARY libpng15 ${LPATH})
 
-FIND_LIBRARY(
-  PNG_LIBRARY 
-  NAMES 
-    libpng15
-  PATHS
-    ${PNG_ROOT}/lib/${KINEVOX_ARCHITECTURE}
-  NO_DEFAULT_PATHS )
-
-SET(libpng_INCLUDE_DIR ${PNG_INCLUDE_DIR} ${zlib_INCLUDE_DIR})
-list(APPEND libpng_LIBS ${zlib_LIBS} )
-list(APPEND libpng_LIBS ${PNG_LIBRARY})
-
-#SET(libpng_LIBS ${PNG_LIBRARY} ${zlib_LIBS})
-
+		
+	SET(libpng_INCLUDE_DIR ${PNG_INCLUDE_DIR} ${zlib_INCLUDE_DIR})
+	list(APPEND libpng_LIBS ${zlib_LIBS} )
+	list(APPEND libpng_LIBS ${PNG_LIBRARY})
+	#message("png!!!" ${PNG_LIBRARY})
 ELSE()
-FIND_PACKAGE(PNG)
-SET(libpng_INCLUDE_DIR ${PNG_INCLUDE_DIR})
-list(APPEND libpng_LIBS ${PNG_LIBRARY})
-
+	FIND_PACKAGE(PNG)
+	SET(libpng_INCLUDE_DIR ${PNG_INCLUDE_DIR})
+	list(APPEND libpng_LIBS ${PNG_LIBRARY})
 ENDIF()
 

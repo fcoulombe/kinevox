dc89496b9853a93e74e36b9eace2727f140f7ee8
fcoulombe <fcoulombe@silentfalls.org>
2013-07-05 18:19:48 -0300

    franky creates the base of the threading model for the renderer. basically we have a rende rpipe that has render threads and we emit rendercommands to this pipe that will dispatch to the threads

---
+++ lib/renderer/renderer/RenderThread.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2013 by Francois Coulombe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+#include <iostream>
+#include <queue>
+#include <gcl/Thread.h>
+#include <gcl/Mutex.h>
+#include <gcl/Semaphore.h>
+#include "renderer/RenderCmd.h"
+namespace GCL
+{
+struct RenderData;
+class RenderThread : public Thread
+{
+public:
+	RenderThread()
+	{
+		Start();
+	}
+	virtual ~RenderThread() {}
+	void SendCommand(RenderCommand *cmd)
+	{
+		mMutex.Lock();
+		mCommandList.push(cmd);
+		mMutex.Unlock();
+		mRunMutex.Notify();
+	}
+private:
+	void Run()
+	{
+		while (mIsRunning)
+		{
+			mRunMutex.Wait();
+			std::cout <<"render"<<std::endl;
+			RenderCommand* cmd =nullptr;
+			mMutex.Lock();
+			if (!mCommandList.empty())
+			{
+				cmd = mCommandList.front();
+				mCommandList.pop();
+			}
+			mMutex.Unlock();
+			if (cmd)
+			{
+				mRenderCommandMap[cmd->mCmd](cmd->mData, *mRenderData);
+					//cmd->Run(mRenderData);
+					delete cmd;
+			}
+		}
+	}
+	std::queue<RenderCommand*> mCommandList;
+	Semaphore mRunMutex;
+	Mutex mMutex;
+protected:
+	RenderCommandFunction *mRenderCommandMap; //vtable for render commands. configured by child class
+	RenderData *mRenderData; //holds client side render data
+};
+
+
+}

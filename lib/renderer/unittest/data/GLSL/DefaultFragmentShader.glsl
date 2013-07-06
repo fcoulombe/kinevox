49f2f41457a507aa7f992c76e517662dfb240c92
fcoulombe <fcoulombe@silentfalls.org>
2013-07-06 11:27:46 -0300

    franky adds test shader files

---
+++ lib/renderer/unittest/data/GLSL/DefaultFragmentShader.glsl
@@ -0,0 +1,11 @@
+uniform sampler2D texture; 	
+varying vec2 texcoord;	
+varying vec4 color;	
+void main() 
+{
+#if ENABLE_FIX_PIPELINE 
+	gl_FragColor = texture2D( texture, gl_TexCoord[0].st ); 
+#else 
+	gl_FragColor = texture2D(texture, texcoord);
+#endif 
+}

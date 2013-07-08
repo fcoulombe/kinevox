d0e4286ea36bf487bceb682c8e1f6bcf1a6ee27b
fcoulombe <fcoulombe@silentfalls.org>
2013-07-08 02:45:45 -0300

    franky adds a pipeline for the archetype which will be the actor templates

---
+++ cmake/pipeline/Archetype.cmake
@@ -0,0 +1,25 @@
+SET(OUT_SCRIPT_DIR ${CMAKE_CURRENT_BINARY_DIR}/data/Archetype/)
+file(MAKE_DIRECTORY ${OUT_SCRIPT_DIR})
+file(GLOB scriptFiles ${dataPath}*.arch)
+IF(${WIN32})
+IF(${KINEVOX_ARCHITECTURE} MATCHES "32bit")
+	SET(LUACOMPILER_EXE ${PROJECT_SOURCE_DIR}/3rdParty/lua/bin/luac.exe)
+ELSE()
+	SET(LUACOMPILER_EXE ${PROJECT_SOURCE_DIR}/3rdParty/lua/bin/luac64.exe)
+ENDIF()
+ELSE()
+SET(LUACOMPILER_EXE luac)
+ENDIF()
+foreach(src ${scriptFiles})
+	get_filename_component(SrcExt ${src} EXT)
+	string(REPLACE ${SrcExt} ".arch" DstFile ${src})
+	string(REPLACE ${dataPath} ${OUT_SCRIPT_DIR} DstFile ${DstFile})
+
+	add_custom_command(
+		OUTPUT ${DstFile}
+		COMMAND ${LUACOMPILER_EXE} -o ${DstFile} ${src} 
+		DEPENDS ${SrcFile} )
+	list(APPEND DstFiles ${DstFile})
+endforeach()
+SET(DATA_DEP ${DATA_DEP} ${DstFiles})
+#add_custom_target(MeshBuilder  DEPENDS ${DstFiles})

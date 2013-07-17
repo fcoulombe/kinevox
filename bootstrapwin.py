9b5876c158d203e453ca668c44b598f8d1508067
fcoulombe <fcoulombe@silentfalls.org>
2013-07-17 19:45:04 -0300

    update the bootstrap to support 3rd party branch

--- bootstrapwin.py
+++ bootstrapwin.py
@@ -6,7 +6,11 @@ import os.path
 def main():
 	if not os.path.exists("lib/gcl"):
 		os.system("git clone https://github.com/fcoulombe/gcl.git lib/gcl")
-	os.system("python tools/Install3rdparty.py")
+	if not os.path.exists("3rdParty"):
+		os.system("git clone https://github.com/fcoulombe/kinevox_3rdparty.git 3rdParty")
+		os.chdir("3rdParty")
+		os.system("python clone.py")
+		os.chdir("..")
 
 if __name__ == "__main__":
 	main()

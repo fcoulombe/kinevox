f81e33f7e536d0640915de9eef77c1cd12a8814b
fcoulombe <fcoulombe@silentfalls.org>
2014-11-20 23:46:50 -0300

    Removing useless commented code to the shader

--- program/example/example_data/data/GLSL/DefaultSpriteFragmentShader.glsl
+++ program/example/example_data/data/GLSL/DefaultSpriteFragmentShader.glsl
@@ -4,7 +4,5 @@ varying vec2 texcoord;
 
 void main() 
 {
-	vec4 col  = texture2D(texture, texcoord);
-	gl_FragColor = col;
-	//gl_FragColor= vec4(texcoord.x, 0, texcoord.y, 1);
+	gl_FragColor = texture2D(texture, texcoord);;
 }

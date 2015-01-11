1480dd742660aad861ee4ecfe2d87161d54fefd4
fcoulombe <fcoulombe@silentfalls.org>
2015-01-11 17:06:09 -0300

    franky scales the game to work in 512

--- program/crashcar/data/Script/MainGameState.lua
+++ program/crashcar/data/Script/MainGameState.lua
@@ -1,52 +1,60 @@
+MINE_COUNT = 9
+VELOCITY = { -0.0,50.0} 
 function Initialize(self)
   kinevox.Log("Main Game State!")
   SCREEN_SIZE = kinevox.GetScreenSize()
+  HALF_SCREEN_WIDTH = SCREEN_SIZE[0]/2
+  HALF_SCREEN_HEIGHT = SCREEN_SIZE[1]/2
     
   kinevox.CreateActor("Road1", "Road")
   local road = kinevox.GetActor("Road1")
-  road:SetPosition(SCREEN_SIZE[0]/2, SCREEN_SIZE[1]/2, 0)
+  road:SetPosition(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT, 0.0)
   
   kinevox.CreateActor("Road2", "Road")
   local road = kinevox.GetActor("Road2")
-  road:SetPosition(SCREEN_SIZE[0]/2, SCREEN_SIZE[1]/2 - SCREEN_SIZE[1], 0)
+  road:SetPosition(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT - SCREEN_SIZE[1]+1, 0.0)
   
   kinevox.CreateActor("Car", "Car")
   local car = kinevox.GetActor("Car")
-  car:SetPosition(SCREEN_SIZE[0]/2, SCREEN_SIZE[1]/2 + 100, 1)
+  car:SetPosition(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT + 100, 1)
   
-  for i=1,9 do
+  for i=1,MINE_COUNT do
     local mineName = "LMine"..i
     kinevox.CreateActor(mineName, "Mine")
     local mine = kinevox.GetActor(mineName)
-    mine:SetPosition(SCREEN_SIZE[0]/2-120,  (i-1)*64, 1)
+    mine:SetPosition(HALF_SCREEN_WIDTH-120,  (i-1)*64-32, 0.5)
   end
-  for i=1,9 do
+  for i=1,MINE_COUNT do
     local mineName = "RMine"..i
     kinevox.CreateActor(mineName, "Mine")
     local mine = kinevox.GetActor(mineName)
-    mine:SetPosition(SCREEN_SIZE[0]/2+120,  (i-1)*64, 1)
+    mine:SetPosition(HALF_SCREEN_WIDTH+120,  (i-1)*64-32, 0.5)
   end
 end
 
 function Logic(self, dt)
+  --kinevox.Log("dt: "..dt.." Velocity: "..VELOCITY[1].." "..VELOCITY[2]) 
   local road1 = kinevox.GetActor("Road1")
   local roadPos = road1:GetPosition()
-  roadPos[1] = roadPos[1] + 1.0
-  if roadPos[1] > SCREEN_SIZE[1]/2 + SCREEN_SIZE[1] then
-    roadPos[0] = SCREEN_SIZE[0]/2
-    roadPos[1] = SCREEN_SIZE[1]/2 - SCREEN_SIZE[1]
-    roadPos[2] = 0
+  roadPos[1] = roadPos[1] + VELOCITY[2] * dt
+  if roadPos[1] > HALF_SCREEN_HEIGHT + SCREEN_SIZE[1]-1 then
+    roadPos[0] = HALF_SCREEN_WIDTH
+    roadPos[1] = HALF_SCREEN_HEIGHT - SCREEN_SIZE[1]
+    roadPos[2] = roadPos[2]
+    kinevox.Log("road1 pos: "..roadPos[0].." "..roadPos[1])
   end
   road1:SetPosition(roadPos[0], roadPos[1], roadPos[2])
   
   
   local road2 = kinevox.GetActor("Road2")
   local roadPos = road2:GetPosition()
-  roadPos[1] = roadPos[1] + 1.0
-  if roadPos[1] > SCREEN_SIZE[1]/2 + SCREEN_SIZE[1] then
-    roadPos[0] = SCREEN_SIZE[0]/2
-    roadPos[1] = SCREEN_SIZE[1]/2 - SCREEN_SIZE[1]
-    roadPos[2] = 0
+  roadPos[1] = roadPos[1] + VELOCITY[2] * dt
+  if roadPos[1] > HALF_SCREEN_HEIGHT + SCREEN_SIZE[1]-1 then
+    roadPos[0] = HALF_SCREEN_WIDTH
+    roadPos[1] = HALF_SCREEN_HEIGHT - SCREEN_SIZE[1]
+    roadPos[2] = roadPos[2]
+    
+    kinevox.Log("road2 pos: "..roadPos[0].." "..roadPos[1])
   end
   road2:SetPosition(roadPos[0], roadPos[1], roadPos[2])
   
@@ -56,7 +64,7 @@ function Terminate(self)
   kinevox.DestroyActor("Car")
   kinevox.DestroyActor("Road2")
   kinevox.DestroyActor("Road1")
-  for i=1,9 do
+  for i=1,MINE_COUNT do
     local mineName = "RMine"..i
     kinevox.DestroyActor(mineName)
     local mineName = "LMine"..i
--- program/crashcar/data/Script/Mine.lua
+++ program/crashcar/data/Script/Mine.lua
@@ -1,22 +1,22 @@
 function Initialize(self)
     SCREEN_SIZE = kinevox.GetScreenSize()
-    VELOCITY = { -0.0,1.0} 
+    VELOCITY = { -0.0,50.0} 
 end
 
 function Logic(self, dt)
-   
+    --kinevox.Log("dt: "..dt.." Velocity: "..VELOCITY[1].." "..VELOCITY[2]) 
     pos = self:GetPosition()
    
     local newX = pos[0]
     local newY = pos[1]    
      
-    local velocityX = VELOCITY[1]
-    local velocityY = VELOCITY[2]
+    local velocityX = VELOCITY[1] * dt
+    local velocityY = VELOCITY[2] * dt
     newX = newX + velocityX 
     newY = newY + velocityY 
     
-    if newY > SCREEN_SIZE[1] then
-      newY = 0
+    if newY > SCREEN_SIZE[1]+32+1 then
+      newY = -32
     end
     
     
@@ -24,7 +24,7 @@ function Logic(self, dt)
     local car = kinevox.GetActor("Car")
     local carPos = car:GetPosition()
     
-    self:SetPosition(newX, newY, 0)
+    self:SetPosition(newX, newY, pos[2])
 end
 
 
